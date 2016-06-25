2a9416ed3f858cdf6a0ab8696c161911a003e58c
leeSmile <244645877@qq.com>
2016-06-25 21:37:02 +0800

    text111??

---
+++ textTools/textTools/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  textTools
+//
+//  Created by Lee on 16/6/25.
+//  Copyright © 2016年 Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ textTools/textTools/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  textTools
+//
+//  Created by Lee on 16/6/25.
+//  Copyright © 2016年 Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
